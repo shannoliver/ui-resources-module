ee2665bf7d5c3084d679721d1f453f601044d935
Sriram Srinivasan <sriram@dhcp-10-142-236-168.uawifi.arizona.edu>
2013-03-18 16:58:42 -0700

    update pom for gxt3 branch. add static ref to resources

--- src/main/java/org/iplantc/core/resources/client/IplantResources.java
+++ src/main/java/org/iplantc/core/resources/client/IplantResources.java
@@ -3,10 +3,22 @@
  */
 package org.iplantc.core.resources.client;
 
+import org.iplantc.core.resources.client.images.Icons;
+import org.iplantc.core.resources.client.messages.IplantDisplayStrings;
+import org.iplantc.core.resources.client.messages.IplantErrorStrings;
+
+import com.google.gwt.core.shared.GWT;
+
 /**
  * @author sriram
  *
  */
 public class IplantResources {
 
+	public static IplantDisplayStrings DISPLAY = GWT.create(IplantDisplayStrings.class);
+	
+	public static IplantErrorStrings ERROR = GWT.create(IplantErrorStrings.class);
+	
+	public static Icons 	ICON = GWT.create(Icons.class);
+	
 }
