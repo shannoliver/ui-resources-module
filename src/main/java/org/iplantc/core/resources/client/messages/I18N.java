960f2973e5a1c14f3d8d188f50e8c21fb9727ed6
Paul Sarando <psarando@iplantcollaborative.org>
2013-04-17 17:24:23 -0700

    Imported IplantValidationMessages from ui-apps-widgets.

--- src/main/java/org/iplantc/core/resources/client/messages/I18N.java
+++ src/main/java/org/iplantc/core/resources/client/messages/I18N.java
@@ -13,5 +13,5 @@ public class I18N {
 
 	public final static IplantDisplayStrings DISPLAY = GWT.create(IplantDisplayStrings.class);
 	public static final IplantErrorStrings  ERROR = GWT.create(IplantErrorStrings.class);
-
+    public static final IplantValidationMessages RULES = GWT.create(IplantValidationMessages.class);
 }
