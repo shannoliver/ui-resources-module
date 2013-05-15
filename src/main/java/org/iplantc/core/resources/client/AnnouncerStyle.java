61beaaea525499c0d75be425ab077bd3e6734dc5
Paul Sarando <psarando@iplantcollaborative.org>
2013-05-14 18:01:29 -0700

    Applied standard UI code style to AnnouncerStyle.

--- src/main/java/org/iplantc/core/resources/client/AnnouncerStyle.java
+++ src/main/java/org/iplantc/core/resources/client/AnnouncerStyle.java
@@ -7,40 +7,39 @@ import com.google.gwt.resources.client.CssResource;
  */
 public interface AnnouncerStyle extends CssResource {
 
-	/**
-	 * The close button styling when the mouse is not over the button.
-	 * 
-	 * @return the style name
-	 */
-	String closeButton();
+    /**
+     * The close button styling when the mouse is not over the button.
+     * 
+     * @return the style name
+     */
+    String closeButton();
 
-	/**
-	 * The close button styling when the mouse is over the button.
-	 * 
-	 * @return the style name
-	 */
-	String closeButtonOver();
+    /**
+     * The close button styling when the mouse is over the button.
+     * 
+     * @return the style name
+     */
+    String closeButtonOver();
 
-	/**
-	 * The styling applied to the main announcer panel.
-	 * 
-	 * @return the style name
-	 */
-	String panel();
+    /**
+     * The styling applied to the main announcer panel.
+     * 
+     * @return the style name
+     */
+    String panel();
 
-	/**
-	 * The additive styling applied to the main announcer panel when there are
-	 * multiple announcements.
-	 * 
-	 * @return the additive style name
-	 */
-	String panelMultiple();
+    /**
+     * The additive styling applied to the main announcer panel when there are multiple announcements.
+     * 
+     * @return the additive style name
+     */
+    String panelMultiple();
 
-	/**
-	 * The styling applied to the content widget area of the panel.
-	 * 
-	 * @return the style name
-	 */
-	String content();
+    /**
+     * The styling applied to the content widget area of the panel.
+     * 
+     * @return the style name
+     */
+    String content();
 
 }
