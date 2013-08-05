de3254ab1509269fd73e70819bca46058652dc85
Jonathan Strootman <jstroot@iplancollaborative.org>
2013-08-05 15:52:52 -0700

    CORE-4015: * Added overriding styles for IPlant Windows and content panels which set overflow styles for text

---
+++ src/main/java/org/iplantc/core/resources/client/theme/window/IPlantGrayWindowAppearance.java
@@ -0,0 +1,21 @@
+package org.iplantc.core.resources.client.theme.window;
+
+import com.google.gwt.core.client.GWT;
+import com.sencha.gxt.theme.base.client.widget.HeaderDefaultAppearance;
+import com.sencha.gxt.theme.gray.client.window.GrayWindowAppearance;
+
+public class IPlantGrayWindowAppearance extends GrayWindowAppearance implements IPlantWindowAppearance {
+
+    public interface IPlantGrayHeaderResources extends GrayHeaderResources {
+
+        @Override
+        @Source({"com/sencha/gxt/theme/base/client/widget/Header.css", "com/sencha/gxt/theme/gray/client/window/GrayWindowHeader.css", "IPlantGrayWindowHeader.css"})
+        GrayHeaderStyle style();
+
+    }
+
+    @Override
+    public HeaderDefaultAppearance getHeaderAppearance() {
+        return new HeaderDefaultAppearance(GWT.<IPlantGrayHeaderResources> create(IPlantGrayHeaderResources.class));
+    }
+}
---
+++ src/main/java/org/iplantc/core/resources/client/theme/window/IPlantWindowAppearance.java
@@ -0,0 +1,7 @@
+package org.iplantc.core.resources.client.theme.window;
+
+import com.sencha.gxt.widget.core.client.Window.WindowAppearance;
+
+public interface IPlantWindowAppearance extends WindowAppearance {
+
+}
